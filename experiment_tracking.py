00db5268fc366dc87df9b64cf2848a4c1b9ba712
JiayiD77 <dongjiayi9527@gmail.com>
2024-02-04 17:24:27 -0800

    Update experiment_tracking.py

--- experiment_tracking.py
+++ experiment_tracking.py
@@ -5,6 +5,7 @@ from torch import nn
 from torchinfo import summary
 from torch.utils.tensorboard import SummaryWriter
 from tqdm.auto import tqdm
+from datetime import datetime
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -20,6 +21,15 @@ def set_seeds(seed: int=42):
 def check_image_data_property(image_data_point, class_names):
     print(f'Single image shape: {image_data_point[0].shape}')
     print(f'Image Class: {class_names[image_data_point[1]]}')
+
+def create_writer(experiment_name, model_name, extra=None):
+    time_stamp = datetime.now().strftime("%Y-%m-%d-%H-%M")
+    if extra:
+         log_dir = os.path.join('runs', time_stamp, experiment_name, model_name, extra)
+    else:
+        log_dir = os.path.join('runs', time_stamp, experiment_name, model_name)
+    writer = SummaryWriter(log_dir=log_dir)
+    return writer
 
 def main():
     # Import model
@@ -77,8 +87,11 @@ def main():
     # Train
     set_seeds()
 
-    model.train()
-    for epoch in tqdm(range(10)):
+    Epochs = 5
+    writer = create_writer('experiment_tracking', 'EfficientNet_B2', '10_epoch')
+    
+    for epoch in tqdm(range(Epochs)):
+        model.train()
         train_loss, train_acc = 0, 0
         for batch, (X, y) in enumerate(train_data_loader):
             X, y = X.to(device), y.to(device)
@@ -93,22 +106,26 @@ def main():
         train_loss = train_loss / len(train_data_loader)
         train_acc = train_acc / len(train_data_loader)
         print(f'Epoch {epoch}:\n')
-        print(f'Train loss: {train_loss} | Train acc: {train_acc}\n')
+        print(f'Train loss: {train_loss:.4f} | Train acc: {train_acc:.4f}\n')
+        
+        model.eval()
+        test_loss, test_acc = 0, 0
+        with torch.inference_mode():
+            for batch, (X, y) in enumerate(test_data_loader):
+                X, y = X.to(device), y.to(device)
+                y_pred = model(X)
+                loss = loss_fn(y_pred, y)
+                test_loss += loss.item()
+                y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+                test_acc += (y_pred_class == y).sum().item() / len(y)
+            test_loss = test_loss / len(test_data_loader)
+            test_acc = test_acc / len(test_data_loader)
+            print(f'Test loss: {test_loss:.4f} | Test acc: {test_acc:.4f}\n')
 
-    model.eval()
-    test_loss, test_acc = 0, 0
-    with torch.inference_mode():
-        for batch, (X, y) in enumerate(test_data_loader):
-            X, y = X.to(device), y.to(device)
-            y_pred = model(X)
-            loss = loss_fn(y_pred, y)
-            test_loss += loss.item()
-            y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
-            test_acc += (y_pred_class == y).sum().item() / len(y)
-        test_loss = test_loss / len(test_data_loader)
-        test_acc = test_acc / len(test_data_loader)
-        print(f'Test loss: {test_loss} | Test acc: {test_acc}\n')
-    
+            writer.add_scalars(main_tag='Loss', tag_scalar_dict={'train_loss':train_loss, 'test_loss':test_loss}, global_step=epoch)
+            writer.add_scalars(main_tag='Accuracy', tag_scalar_dict={'train_acc':train_acc, 'test_acc':test_acc}, global_step=epoch)
+        
+    writer.close()
 
 if __name__=="__main__":
     main()
